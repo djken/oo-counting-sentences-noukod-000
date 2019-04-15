4e775265e5963bfb91a300e14875fec0e83e5104
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-15 17:40:30 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,9 @@ class String
 
   def count_sentences
     element_count = self.split(/[.?!]/)
-    element_count.delete_if{|element| element.empty?}
+    element_count.delete_if do |element| 
+      element.empty?}
+    end
     element_count.length
   end
 end
